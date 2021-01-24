2342772eae971a72ee922751fceaa3f938ef65fb
Ryan Delaney <vitineth@gmail.com>
2021-01-24 23:35:30 +0000

    Documenting client facing error

--- src/errors/ClientFacingError.ts
+++ src/errors/ClientFacingError.ts
@@ -1,3 +1,7 @@
+/**
+ * Represents an error which can be displayed directly to the user, this means that it contains no information which can
+ * compromise user information or the system
+ */
 export class ClientFacingError extends Error {
 
     constructor(message: string) {
