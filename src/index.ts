05bab4cc98b987ac46aafd354ca4ec9a5b4db07d
Ryan Delaney <vitineth@gmail.com>
2021-01-24 21:51:48 +0000

    Exporting the rest of the modules

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,13 @@
 export * from './database/GenericDatabase';
 export * from './database/GenericMongoDatabase';
+export * from './database/GenericCommentDatabase';
+
+export * from './errors/ClientFacingError';
+
+export * from './healthcheck/Healthcheck';
+
 export * from './messaging/AbstractBrokerHandler';
 export * from './messaging/GenericRabbitNetworkHandler';
+
+export * from './utility/CommentBinder';
+export * from './utility/GenericDatabaseFunctions';
